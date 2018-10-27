c03ba36f6ce8426bd386a864f58e8f3180c30a21
shubham <aggarwal.sh02@gmail.com>
2018-10-27 17:19:40 +0530

    added using bubble sort

--- Pair_Sum.java
+++ Pair_Sum.java
@@ -2,14 +2,38 @@
 import java.util.Arrays;
 class Pair_Sum
 {
+	static void bsort(int a[],int n)
+		{
+			for(int i=0;i<n;i++)
+			{
+				for(int j=0;j<n;j++)
+				{
+					if(a[j]>a[j+1])
+					{
+						int tmp=a[j];
+						a[j]=a[j+1];
+						a[j+1]=tmp;
+					}
+					
+				}
+			}
+			
+		}	
+		
+		
 	public static void main(String [] args)
 	{
 	
-		int arr[]={0,2,3,5,6,7,9,21,43,52,53,54,55,56,57,58,59,60};
+		int arr[]={0,3,2,5,6,7,9,21,43,52,53,54,55,56,57,58,59,60};
 		int ans=60;
-		//Array.sort(arr);
+		
+		
 		int j=0,n=arr.length-1;
 		System.out.println("total nos in your array are "+n);
+		bsort(arr,n);
+		System.out.println("array is sorted now ");
+		for(int i=0;i<n;i++)
+			System.out.println(arr[i]);
 		for(int i=0;i<arr.length;i++)
 		{
 				if(	(arr[j]+arr[n])<ans)
