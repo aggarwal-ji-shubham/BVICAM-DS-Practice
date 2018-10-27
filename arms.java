339cd746306bb66da16897deae43894476a53409
shubham <aggarwal.sh02@gmail.com>
2018-10-27 15:44:06 +0530

    added

---
+++ arms.java
@@ -0,0 +1,26 @@
+// to find a no is armstrong or not
+import java.util.Scanner;
+class arms
+{
+	public static void main(String args[])
+	{
+			System.out.println("enter a no");
+			Scanner in=new Scanner(System.in);
+			int num=in.nextInt();
+			int tmp=num,rev=0;
+			while(num!=0)
+			{
+				int rem=num%10;
+				rev=rev+rem*rem*rem;
+				num=num/10;
+				
+				
+			}
+		if(tmp==rev)
+			System.out.println("Armstrong ");
+		else
+			System.out.println(" Not Armstrong ");
+		
+		
+	}
+}
